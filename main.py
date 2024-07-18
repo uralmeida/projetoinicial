8c87419c6b6e5c9fc270faa9b4819903bf93efef
Jeferson Almeida <jeferson.live11@gmail.com>
2024-07-17 22:46:33 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Olá")
+print("Jornada de dados")
+
+# Variáveis
+idade = 24
+nome = "Jeferson"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Metódos
+lista_invertida = lista.reverse()
